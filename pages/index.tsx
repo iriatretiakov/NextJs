eaab2c37e8406029eda81de721347ef6d458fcdb
ilyat <ilya.tretyakov@velaskec.com>
2023-01-10 20:24:42 +0900

    nextjs edu init commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import { Inter } from '@next/font/google';
+import styles from '../styles/Home.module.css';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
-export default function Home() {
+export default function Home(): JSX.Element {
+  const a = 1;
   return (
     <>
       <Head>
@@ -111,7 +112,9 @@ export default function Home() {
             <h2 className={inter.className}>
               Deploy <span>-&gt;</span>
             </h2>
+            {a}
             <p className={inter.className}>
+
               Instantly deploy your Next.js site to a shareable URL
               with&nbsp;Vercel.
             </p>
@@ -119,5 +122,5 @@ export default function Home() {
         </div>
       </main>
     </>
-  )
+  );
 }
